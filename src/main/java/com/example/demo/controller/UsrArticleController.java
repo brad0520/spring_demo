72642d7982d6821bb7244a282cac5cc6c27bc41d
NTL <58549685+brad0520@users.noreply.github.com>
2021-03-27 00:20:29 +0900

    doAdd Article update

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -1,8 +1,9 @@
 package com.example.demo.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-
+import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,30 +12,49 @@ import com.example.demo.dto.Article;
 
 //http:localhost:8024/usr/article/detail
 //http:localhost:8024/usr/article/list
+//http://localhost:8024/usr/article/doAdd?regDate=2021-03-26 22:47:32&title=제목3&body=내용3
 
 @Controller
 public class UsrArticleController {
 
+	private int articlesLastId;
 	private List<Article> articles;
 	
 	public UsrArticleController() {
-		articles =new ArrayList<>();
+		// 맴버변수 초기화 
+		articlesLastId = 0;
+		articles = new ArrayList<>();
 		
-		articles.add(new Article(1, "2021-03-26 21:31:45", "제목1", "내용1"));
-		articles.add(new Article(2, "2021-03-26 21:32:52", "제목2", "내용2"));
+		// 게시물 2개 생성 
+		articles.add(new Article(++articlesLastId, "2021-03-26 21:31:45", "제목1", "내용1"));
+		articles.add(new Article(++articlesLastId, "2021-03-26 21:32:52", "제목2", "내용2"));
 	}
-	
+	// 게시물 상세보기 	
 	@RequestMapping("/usr/article/detail")
 	@ResponseBody
 	public Article showDetail(int id) {
 		
 		return articles.get(id-1);
 	}
-
+	// 게시물 리스트 보기 
 	@RequestMapping("/usr/article/list")
 	@ResponseBody
 	public List<Article> showList() {
 		
 		return articles;
+	}
+	// 게시물 추가 
+	@RequestMapping("/usr/article/doAdd")
+	@ResponseBody
+	public Map<String, Object> doAdd(String regDate, String title, String body) {
+		
+		articles.add(new Article(++articlesLastId, regDate, title, body));
+		
+		Map<String, Object> rs = new HashMap<>();
+		rs.put("resultCode", "S-1");
+ 		rs.put("msg", "성공하였습니다.");
+		rs.put("id", articlesLastId);
+		
+		return rs;
 	}
 }
